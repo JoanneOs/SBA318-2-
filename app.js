8dbe7a11a921b210f665e57f1edf596f8a377a85
JoanneOs <jowan.awsman@yahoo.com>
2025-04-11 12:47:52 -0500

    Tell it to listen for incoming requests

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 
 const express = require("express"); // bring in express so we can use it
 const app = express(); // this creates our express application
+
+
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+  });
+  
