73cb44b0d5cf04c8b48aeadd4aefb55297afbd5f
JoanneOs <jowan.awsman@yahoo.com>
2025-04-11 13:04:46 -0500

    middleware

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 const express = require("express"); // bring in express so we can use it
 const app = express(); // this creates our express application
 
+//adding middleware const
+const logger = require("./middleware/logger");
+const checkUser = require("./middleware/checkUser");
+
+
 //telling  my browser where to view
 const path = require("path");
 app.set("views", path.join(__dirname, "views"));
@@ -11,8 +16,15 @@ app.set("view engine", "ejs"); //setting view engine
 
 app.use(express.static("public"));
 
+
+
 app.use(express.json()); // for JSON requests
 app.use(express.urlencoded({ extended: true })); // for form submissions
+
+//using middleware
+app.use(logger); // for all routes
+app.use("/users", checkUser); // only for user routes
+
 
 
 //changing home view:  
